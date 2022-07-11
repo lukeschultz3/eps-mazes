02941d5d70646f13d200c14cf9647b606cf00dc1
luke3 <luke3@ualberta.ca>
2022-07-11 11:21:13 -0600

    Get working!!

--- maze-to-eps.py
+++ maze-to-eps.py
@@ -9,12 +9,73 @@
 # last edited on July 5, 2022
 
 
-cell_radius = 10
+maze = [["x", "x", "x", "x", "x", "x", "x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+["x", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", "x"],
+["x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x", " ", "x"],
+["x", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", " ", " ", " ", " ", "x"],
+["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x", "x", "x", " ", "x"],
+["x", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", " ", " ", "x"],
+["x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x", "x", "x"],
+["x", " ", "x", " ", " ", " ", "x", " ", " ", " ", " ", " ", "x", " ", "x", " ", "x"],
+["x", " ", "x", " ", "x", " ", "x", " ", "x", "x", "x", " ", "x", " ", "x", " ", "x"],
+["x", " ", "x", " ", "x", " ", " ", " ", "x", " ", " ", " ", "x", " ", " ", " ", "x"],
+["x", " ", "x", "x", "x", "x", "x", " ", "x", " ", "x", "x", "x", " ", "x", " ", "x"],
+["x", " ", " ", " ", " ", " ", "x", " ", "x", " ", " ", " ", " ", " ", "x", " ", "x"],
+["x", "x", "x", "x", "x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x"],
+["x", " ", " ", " ", "x", " ", " ", " ", "x", " ", " ", " ", " ", " ", "x", " ", "x"],
+["x", " ", "x", " ", "x", " ", "x", " ", "x", "x", "x", "x", "x", " ", "x", " ", "x"],
+["x", " ", "x", " ", " ", " ", "x", " ", " ", " ", "x", " ", " ", " ", " ", " ", "x"],
+["x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x", "x", "x", "x", "x", "x", "x"]]
+
+cell_length = 10
 
 def print_head(rows, cols):
     print("%!PS-Adobe-3.0 EPSF-3.0")
-    print("%%BoundingBox: 0 0", cols*2*cell_radius, rows*2*cell_radius)
+    print("%%BoundingBox: 0 0", cols*cell_length, rows*cell_length)
+    print("%%Pages: 0")
+    print("%%EndComments")
 
+def print_wall(row, col):
+    print("newpath")
+    print(col*cell_length, row*cell_length, "moveto")
+    print((col+1)*cell_length, row*cell_length, "lineto")
+    print((col+1)*cell_length, (row+1)*cell_length, "lineto")
+    print(col*cell_length, (row+1)*cell_length, "lineto")
+    print("closepath")
+    print("fill")
+
+def print_wall_vert(row, col):
+    print("newpath")
+    print(col*cell_length, row*cell_length, "moveto")
+    print(col*cell_length, (row-1)*cell_length, "lineto")
+    print("closepath")
+    print("stroke")
+
+def print_wall_hori(row, col):
+    print("newpath")
+    print(col*cell_length, row*cell_length, "moveto")
+    print((col+1)*cell_length, row*cell_length, "lineto")
+    print("closepath")
+    print("stroke")
 
 if __name__=="__main__":
-    print_head(20, 20)
+    print_head(len(maze)//2, len(maze[0])//2)
+
+    total_rows = len(maze)//2
+
+    row = 0
+    col = 0
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            row = i // 2
+            col = j // 2
+            if maze[i][j] == "x":
+                if i % 2 == 0 and j % 2 == 0:
+                    continue
+                if i % 2 == 0:
+                    print_wall_hori(total_rows-row, col)
+                elif j % 2 == 0:
+                    print_wall_vert(total_rows-row, col)
+
+    print("showpage")
+
